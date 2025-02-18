a36021bb7fdceb933d0e9d6c11a5ccb0110cf2d5
sudheerv45 <sudheer.v@oneehr.net>
2025-02-18 12:30:54 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      connectTimeoutMS: 30000
+
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
